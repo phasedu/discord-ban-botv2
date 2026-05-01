55f4febc3849cae60acc5ba417dc9791dfd24c8d
phasedu <cogking3@gmail.com>
2026-05-01 15:46:08 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,55 @@
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = '1493758908781826188';
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('warn')
+    .setDescription('Warn a user')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to warn')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for warning')
+        .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName('warnings')
+    .setDescription('View warnings')
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to check')
+        .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName('clearwarn')
+    .setDescription('Remove a specific warning')
+    .addUserOption(option =>
+      option.setName('user').setDescription('User').setRequired(true))
+    .addStringOption(option =>
+      option.setName('id').setDescription('Warning ID (#001)').setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName('clearwarnall')
+    .setDescription('Remove all warnings')
+    .addUserOption(option =>
+      option.setName('user').setDescription('User').setRequired(true)
+    )
+].map(cmd => cmd.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  await rest.put(
+    Routes.applicationCommands(CLIENT_ID),
+    { body: commands }
+  );
+
+  console.log('Slash commands registered');
+})();
