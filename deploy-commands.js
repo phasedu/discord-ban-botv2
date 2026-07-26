ee5d74dd9b52df7bf19627929b3468ef34e0f0d6
phasedu <cogking3@gmail.com>
2026-07-27 01:39:59 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,10 @@
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const {
+  REST,
+  Routes,
+  SlashCommandBuilder,
+  ContextMenuCommandBuilder,
+  ApplicationCommandType
+} = require('discord.js');
 
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = '1493758908781826188';
@@ -30,48 +36,48 @@ const commands = [
     .setName('clearwarn')
     .setDescription('Remove a specific warning')
     .addUserOption(option =>
-      option.setName('user').setDescription('User').setRequired(true))
+      option.setName('user')
+        .setDescription('User')
+        .setRequired(true))
     .addStringOption(option =>
-      option.setName('id').setDescription('Warning ID (#001)').setRequired(true)
+      option.setName('id')
+        .setDescription('Warning ID (#001)')
+        .setRequired(true)
     ),
 
   new SlashCommandBuilder()
     .setName('clearwarnall')
     .setDescription('Remove all warnings')
     .addUserOption(option =>
-      option.setName('user').setDescription('User').setRequired(true)
+      option.setName('user')
+        .setDescription('User')
+        .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName('delete')
+    .setDescription('Delete a specific message')
+    .addChannelOption(option =>
+      option
+        .setName('channel')
+        .setDescription('Channel containing the message')
+        .setRequired(true)
     )
+    .addStringOption(option =>
+      option
+        .setName('messageid')
+        .setDescription('ID of the message')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('reason')
+        .setDescription('Reason for deleting the message')
+        .setRequired(false)
+    ),
+
+  new ContextMenuCommandBuilder()
+    .setName('Delete Message')
+    .setType(ApplicationCommandType.Message)
+
 ].map(cmd => cmd.toJSON());
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-(async () => {
-  await rest.put(
-    Routes.applicationCommands(CLIENT_ID),
-    { body: commands }
-  );
-
-  console.log('Slash commands registered');
-})();
-
-new SlashCommandBuilder()
-  .setName('delete')
-  .setDescription('Delete a specific message')
-  .addChannelOption(option =>
-    option
-      .setName('channel')
-      .setDescription('Channel containing the message')
-      .setRequired(true)
-  )
-  .addStringOption(option =>
-    option
-      .setName('messageid')
-      .setDescription('ID of the message')
-      .setRequired(true)
-  )
-  .addStringOption(option =>
-    option
-      .setName('reason')
-      .setDescription('Reason for deleting the message')
-      .setRequired(false)
-  )
